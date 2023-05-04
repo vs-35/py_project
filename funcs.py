4c06b25322844197a622e0a03ae9a13bc6326567
User <eco-64@mail.ru>
2023-05-04 06:42:21 +0500

    function call

--- funcs.py
+++ funcs.py
@@ -2,7 +2,6 @@ def sum_numbers(n1, n2):
     sum = n1 + n2
     return sum
 
-
 def difference_number(n1, n2):
     difference = n1 - n2
     return difference
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-from funcs import sum_numbers
+from funcs import sum_numbers, difference_number
 
 print(sum_numbers(4,5))
+
+print(difference_number(5,4))
